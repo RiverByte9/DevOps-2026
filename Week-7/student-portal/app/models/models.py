f4fc202ee78a24431a40b574dd4ea54d12df2a1b
RiverByte9 <kajalsalgar909@gmail.com>
2026-04-06 12:48:19 +0530

    lint ruff test

--- Week-7/student-portal/app/models/models.py
+++ Week-7/student-portal/app/models/models.py
@@ -1,6 +1,6 @@
 from app import db, login_manager
 from datetime import datetime, timezone
-from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.security import check_password_hash
 from bcrypt import hashpw, checkpw, gensalt
 from flask_login import UserMixin
 
