3c22bd6e5bc8c4d99bb2b787a97ba4d2fd3a3eeb
RiverByte9 <kajalsalgar909@gmail.com>
2026-04-17 10:43:38 +0530

    provider bucket name modification

--- Week-8/infra/providers.tf
+++ Week-8/infra/providers.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "state-bucket-877"
-    key    = "jan26-devops-bootcamp/week8/infra/terraform.tfstate"
+    bucket = "state-bucket-292659698930"
+    key    = "Devops/Week-8/infra/terraform.tfstate"
     region = "us-east-1"
   }
 }
