67be8a131741e92e3477fb393d6da7b259d6dcc1
RiverByte9 <kajalsalgar909@gmail.com>
2026-04-17 10:51:43 +0530

    version modified

--- Week-8/infra/providers.tf
+++ Week-8/infra/providers.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "state-bucket-292659698930"
+    bucket = "state-bucket-29265969893030"
     key    = "Devops/Week-8/infra/terraform.tfstate"
     region = "us-east-1"
   }
